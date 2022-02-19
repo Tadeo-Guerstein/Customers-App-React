05409c33acf19f22a1e70b91a73865060f29fcec
Tadeo Guerstein <tadeleonino@hotmail.com>
2022-02-19 19:00:16 -0300

    added the field component

--- src/components/CustomerEdit.jsx
+++ src/components/CustomerEdit.jsx
@@ -1,19 +1,36 @@
-import React from 'react'
-import PropTypes from 'prop-types'
+import React from 'react';
+import PropTypes from 'prop-types';
+import { reduxForm, Field } from 'redux-form';
+import { connect } from 'react-redux';
 
 const CustomerEdit = ({ name, dni, age }) => {
     return (
         <div>
             <h2>Client Edit</h2>
-            <h3>Name: {name} / DNI: {dni} / Age: {age}</h3>
+            <form action="">
+                <div>
+                    <label htmlFor="name">Name </label>
+                    <Field name="name" component="input" type="text"></Field>
+                </div>
+                <div>
+                    <label htmlFor="dni">DNI: </label>
+                    <Field name="dni" component="input" type="text"></Field>
+                </div>
+                <div>
+                    <label htmlFor="age">Age: </label>
+                    <Field name="age" component="input" type="number"></Field>
+                </div>
+            </form>
         </div>
     )
 }
 
 CustomerEdit.propTypes = {
-    name: PropTypes.string.isRequired, 
-    dni: PropTypes.number.isRequired, 
+    name: PropTypes.string, 
+    dni: PropTypes.string, 
     age: PropTypes.number
 }
 
-export default CustomerEdit
+const CustomerEditForm = reduxForm({ form: 'CustomerEdit' })(CustomerEdit)
+
+export default connect((state, props) => ({ initialValues: props }), null)(CustomerEditForm);

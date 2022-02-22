06964147d1ad2daf470da96d64ee4c4684d655ec
Tadeo Guerstein <tadeleonino@hotmail.com>
2022-02-22 14:48:49 -0300

    added functions for validating the values of the inputs and changed the proptype dni from number to string

--- src/components/CustomerEdit.jsx
+++ src/components/CustomerEdit.jsx
@@ -3,10 +3,7 @@ import PropTypes from 'prop-types';
 import { reduxForm, Field } from 'redux-form';
 import { setPropsAsInitial } from '../helpers/setPropsAsInitial';
 import CustomersActions from './CustomersActions';
-
-// const isRequired = value => (
-//     !value && "This is required"
-// )
+import { Prompt } from 'react-router-dom';
 
 const validate = values => {
     const error = {};
@@ -39,7 +36,17 @@ const MyField = ({ input, meta, type, label, name }) => {
     )
 }
 
-const CustomerEdit = ({ name, dni, age, handleSubmit, submitting, onBack }) => {
+const toNumber = value => value && Number(value)
+
+const toUpper = value => value && value.toUpperCase()
+
+const toLower = value => value && value.toLowerCase()
+
+const onlyGrow = (value, previousValue, values) => 
+    value && (!previousValue ? value : (value > previousValue ? value : previousValue));
+
+const CustomerEdit = ({ 
+    name, dni, age, handleSubmit, submitting, onBack, pristine, submitSucceeded }) => {
     return (
         <div>
             <h2>Client Edit</h2>
@@ -47,14 +54,15 @@ const CustomerEdit = ({ name, dni, age, handleSubmit, submitting, onBack }) => {
                 <Field 
                     name="name" 
                     component={MyField}
-                    label="Name"
+                    label="Name: "
+                    parse={toUpper}
+                    format={toLower}
                 />
 
                 <Field 
                     name="dni" 
                     component={MyField}
-                    validate={isNumber}
-                    label="Dni"
+                    label="Dni: "
                 />
 
                 <Field 
@@ -62,12 +70,21 @@ const CustomerEdit = ({ name, dni, age, handleSubmit, submitting, onBack }) => {
                     component={MyField} 
                     type="number"
                     validate={isNumber}
-                    label="Age"
+                    label="Age: "
+                    parse={toNumber}
+                    normalize={onlyGrow}
                 />
                 <CustomersActions>
-                    <button type="submit" disabled={submitting}>Accept</button>
-                    <button onClick={onBack}>Cancel</button>
+                    <button type="submit" disabled={pristine || submitting}>
+                        Accept
+                    </button>
+                    <button type="button" disabled={submitting} onClick={onBack}>
+                        Cancel
+                    </button>
                 </CustomersActions>
+                <Prompt 
+                    when={!pristine && !submitSucceeded}
+                    message="Your going to lose the changes. Do you want to continue?"/>
             </form>
         </div>
     )

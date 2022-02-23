76d8d06af2e6b5a99c4cd678d9ac0d31c5c59650
Tadeo Guerstein <tadeleonino@hotmail.com>
2022-02-23 11:01:40 -0300

    eliminated a div

--- src/components/CustomerListItem.jsx
+++ src/components/CustomerListItem.jsx
@@ -4,17 +4,15 @@ import PropTypes from 'prop-types';
 
 const CustomerListItem = ({ name, editAction, delAction, urlPath, dni }) => {
     return (
-        <div>
-            <div className="customer-list-item">
-                <div className="field">
-                    <Link to={`${urlPath}${dni}`}>{name}</Link>
-                </div>
-                <div className="field">
-                    <Link to={`${urlPath}${dni}/edit`}>{editAction}</Link>
-                </div>
-                <div className="field">
-                    <Link to={`${urlPath}${dni}/del`}>{delAction}</Link>
-                </div>
+        <div className="customer-list-item">
+            <div className="field">
+                <Link to={`${urlPath}${dni}`}>{name}</Link>
+            </div>
+            <div className="field">
+                <Link to={`${urlPath}${dni}/edit`}>{editAction}</Link>
+            </div>
+            <div className="field">
+                <Link to={`${urlPath}${dni}/del`}>{delAction}</Link>
             </div>
         </div>
     )

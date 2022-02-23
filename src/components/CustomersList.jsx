8c916c874976b4242d401547c589221dd6b0c220
Tadeo Guerstein <tadeleonino@hotmail.com>
2022-02-23 11:01:56 -0300

    added the accessControl HOC for the users permissions

--- src/components/CustomersList.jsx
+++ src/components/CustomersList.jsx
@@ -1,23 +1,23 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 import CustomerListItem from './CustomerListItem';
+import { accessControl } from '../helpers/accessControl';
+import { CUSTOMER_LIST } from '../constants/permissions';
 
 const CustomersList = ({ customers, urlPath }) => {
     return (
-        <div>
-            <div className="customers-list-item">
-                {
-                    customers.map(c => 
-                        c && <CustomerListItem 
-                            key={c.dni}
-                            dni={c.dni}
-                            name={c.name}
-                            editAction={"Edit"}
-                            delAction={"Delete"}
-                            urlPath={urlPath} 
-                        />)
-                }
-            </div>
+        <div className="customers-list-item">
+            {
+                customers.map(c => 
+                    c && <CustomerListItem 
+                        key={c.dni}
+                        dni={c.dni}
+                        name={c.name}
+                        editAction={"Edit"}
+                        delAction={"Delete"}
+                        urlPath={urlPath} 
+                    />)
+            }
         </div>
     )
 }
@@ -27,4 +27,4 @@ CustomersList.propTypes = {
     urlPath: PropTypes.string.isRequired
 }
 
-export default CustomersList
+export default accessControl([CUSTOMER_LIST])(CustomersList)

3d87a7dcf89e19592f132362586299c6d4210255
Tadeo Guerstein <tadeleonino@hotmail.com>
2022-02-23 10:53:12 -0300

    This is the verification of viewing components depending on the permissions of the user

---
+++ src/helpers/accessControl.jsx
@@ -0,0 +1,24 @@
+import React, { Component } from "react";
+import { connect } from 'react-redux';
+
+export const accessControl = permissionsRequired => WrappedComponent => {
+    const SecuredControl = class extends Component{
+        render(){
+            const { permissions } = this.props.user;
+
+            const isAllow = permissionsRequired.every(p => permissions.indexOf(p) >= 0);
+
+            if(!isAllow){
+                return (
+                    <div>
+                        <i>
+                            You dont have permission to access
+                        </i>
+                    </div>
+                )
+            }
+            return <WrappedComponent {...this.props}/>
+        }
+    }
+    return connect(state => ({ user: state.user }))(SecuredControl)
+}
